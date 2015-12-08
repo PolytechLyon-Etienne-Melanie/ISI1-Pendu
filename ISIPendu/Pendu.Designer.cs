4aa639003860b2345a53a82d99e46739939f0e0f
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2015-12-08 13:19:22 +0100

    First commit

---
+++ ISIPendu/Pendu.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ISIPendu
+{
+    partial class Pendu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pendu));
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgv_alphabet = new System.Windows.Forms.DataGridView();
+            this.tsplb_information = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_alphabet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            resources.ApplyResources(this.labelTitle, "labelTitle");
+            this.labelTitle.Name = "labelTitle";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // dgv_alphabet
+            // 
+            this.dgv_alphabet.AllowUserToAddRows = false;
+            this.dgv_alphabet.AllowUserToDeleteRows = false;
+            this.dgv_alphabet.AllowUserToResizeColumns = false;
+            this.dgv_alphabet.AllowUserToResizeRows = false;
+            this.dgv_alphabet.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_alphabet.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv_alphabet.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgv_alphabet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_alphabet.ColumnHeadersVisible = false;
+            resources.ApplyResources(this.dgv_alphabet, "dgv_alphabet");
+            this.dgv_alphabet.Name = "dgv_alphabet";
+            this.dgv_alphabet.ReadOnly = true;
+            this.dgv_alphabet.RowHeadersVisible = false;
+            // 
+            // tsplb_information
+            // 
+            resources.ApplyResources(this.tsplb_information, "tsplb_information");
+            this.tsplb_information.Name = "tsplb_information";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Highlight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView2, "dataGridView2");
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            // 
+            // button
+            // 
+            resources.ApplyResources(this.button, "button");
+            this.button.Name = "button";
+            this.button.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Pendu
+            // 
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.tsplb_information);
+            this.Controls.Add(this.dgv_alphabet);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Pendu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_alphabet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgv_alphabet;
+        private System.Windows.Forms.Label tsplb_information;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ ISIPendu/Pendu.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISIPendu
+{
+    public partial class Pendu : Form
+    {
+        // déclaration et instanciation d’une collection pour le dictionnaire
+        private List<String> dictionnaire = new List<String>();
+
+        public Pendu()
+        {
+            InitializeComponent();
+            initComposants();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        
+
+        private void selectionnerFichier()
+         {
+           OpenFileDialog ofd = new OpenFileDialog();
+          // on initalise la boîte de dialogue
+           ofd.Filter = "txt files (*.txt)|*.txt";
+           ofd.FileName = "";
+           if (ofd.ShowDialog() == DialogResult.OK)
+            {   // on ouvre le fichier
+                FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                String ligne;
+                while ((ligne = sr.ReadLine()) != null)
+                    // Insertion de chaque ligne dans le dictionnaire
+                    dictionnaire.Add(ligne);
+                // Renseignement de la propriété Mot_secret d'un mot tiré au hasard dans le dictionnaire
+                fs.Close();
+                sr.Close();
+                //Mot_secret = dictionnaire[new Random().Next(dictionnaire.Count)];
+                //tsplb_information.Text = "Mot secret chargé, choisissez votre première lettre.";
+                //rafraichirSecret();
+                //bt_verifier.Enabled = true;
+            }
+        }
+
+        // Initialise les composants graphiques du formulaire
+        private void initComposants()
+        {
+            // Charge les cellules du DataGridView
+            dgv_alphabet.ColumnCount = 13;
+            dgv_alphabet.RowCount = 2;
+            dgv_alphabet.Rows[0].Height = 30;
+            dgv_alphabet.Rows[1].Height = 30;
+            for (int i = 0; i < 13; i++)
+            {
+                dgv_alphabet.Columns[i].Width = 30;
+                dgv_alphabet.Rows[0].Cells[i].Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                // Type casting du code ascii (entier) vers sa représentation en caractère (wrapper Char)
+                // Les lettres de l'alphabet se suivent de 65 à 91 dans la table, ce qui rend facile la manipulation
+                dgv_alphabet.Rows[0].Cells[i].Value = ((Char)(i + 65)).ToString();
+                // Traitement de la deuxieme partie de l'alphabet pour la seconde ligne
+                dgv_alphabet.Rows[1].Cells[i].Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                dgv_alphabet.Rows[1].Cells[i].Value = ((Char)(i + (65 + 13))).ToString();
+            }
+            tsplb_information.Text = "Veuillez charger un fichier de dictionnaire de mots.";
+        }
+
+    }
+}
